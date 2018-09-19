1bc6a9e96091c0d77c44c99597eef58135431825
Jinjae Lee <jinjaelee@Administrators-MacBook-Pro.local>
2018-09-19 16:52:01 +0200

    css detail updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,13 @@ const IndexPage = () => (
     <div className='container'>
       <h4>Experiences</h4>
       <div className='content'>
-        <p>Interaction Designer, <a href='http://www.above.se/'>Above</a></p>
+        <p className='title'>Interaction Designer, <a href='http://www.above.se/'>Above</a></p>
         <p className='detail'>2018.03 - now</p>
-        <p>Interactive Art Director, <a href='https://www.hyperisland.com/'>Hyper Island</a></p>
+        <p className='title'>Interactive Art Director, <a href='https://www.hyperisland.com/'>Hyper Island</a></p>
         <p className='detail'>2017.08 - 2018.06</p>
-        <p>Creative Developer, <a href='http://innocean.com/ww-en/'>Innocean</a></p>
+        <p className='title'>Creative Developer, <a href='http://innocean.com/ww-en/'>Innocean</a></p>
         <p className='detail'>2016.11 - 2017.05</p>
-        <p>Digital Producer, <a href='http://innocean.com/ww-en/'>Innocean</a></p>
+        <p className='title'>Digital Producer, <a href='http://innocean.com/ww-en/'>Innocean</a></p>
         <p className='detail'>2015.01 - 2016.11</p>
       </div>
       
@@ -28,13 +28,13 @@ const IndexPage = () => (
     <div className='container'>
       <h4>Works</h4>
       <div className='content'>
-        <p><a href='https://www.youtube.com/watch?v=MfogrhOouZE'>Hyundai i20 WRC Real Play</a></p>
+        <p className='title'><a href='https://www.youtube.com/watch?v=MfogrhOouZE'>Hyundai i20 WRC Real Play</a></p>
         <p className='detail'>UX Design</p>
-        <p><a href='https://www.youtube.com/watch?v=6a97u3tkFQo'>Hyundai IONIQ Explore Plant VR</a></p>
+        <p className='title'><a href='https://www.youtube.com/watch?v=6a97u3tkFQo'>Hyundai IONIQ Explore Plant VR</a></p>
         <p className='detail'>Art Direction</p>
-        <p><a href='#'>CLO:CK:ED:IN</a></p>
+        <p className='title'><a href='#'>CLO:CK:ED:IN</a></p>
         <p className='detail'>Experience Design</p>
-        <p><a href='#'>Sellpy UX Redesign</a></p>
+        <p className='title'><a href='#'>Sellpy UX Redesign</a></p>
         <p className='detail'>Interaction Design</p>
       </div>
     </div>
@@ -42,13 +42,13 @@ const IndexPage = () => (
     <div className='container'>
       <h4>Experiments</h4>
       <div className='content'>
-        <p><a href='https://followmove.netlify.com/'>Follow your move</a></p>
+        <p className='title'><a href='https://followmove.netlify.com/'>Follow your move</a></p>
         <p className='detail'>Two.js</p>
-        <p><a href='#'>Lost in Space</a></p>
+        <p className='title'><a href='#'>Lost in Space</a></p>
         <p className='detail'>Three.js + React</p>
-        <p><a href='#'>Type in Motion</a></p>
+        <p className='title'><a href='#'>Type in Motion</a></p>
         <p className='detail'>Framer</p>
-        <p><a href='#'>Glanceable Animation</a></p>
+        <p className='title'><a href='#'>Glanceable Animation</a></p>
         <p className='detail'>Framer</p>
         
       </div>
@@ -72,5 +72,7 @@ const IndexPage = () => (
 
   </Layout>
 )
+
+
 
 export default IndexPage
