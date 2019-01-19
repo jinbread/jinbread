820ec45356107bef2091f595bfa53f6d3251437d
Jinbread <jinjae.lee@icloud.com>
2019-01-19 23:44:41 +0100

    Portfolio list updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,41 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>Hej!</h1>
-    <h2>Jinjae Lee is a multi-disciplinary interaction designer, make design static to kinetic, blending digital and physical experiences based in Seoul and Stockholm.</h2>
+    <h2>Jinjae Lee is a multi-disciplinary interaction designer, make design static to kinetic, blending digital and physical experiences based in Stockholm.</h2>
+
+
+    
+
+    <div className='container'>
+      <h4>Works</h4>
+      <div className='content'>
+        <p className='title'><Link to="https://www.youtube.com/watch?v=MfogrhOouZE">Hyundai WRC Real Play</Link></p>
+        <p className='detail'>Art Direction, Experience Design</p>
+        <p className='title'><a href='https://www.youtube.com/watch?v=6a97u3tkFQo'>Hyundai IONIQ Explore Plant VR</a></p>
+        <p className='detail'>Art Direction, Experience Design</p>
+        <p className='title'><a href='#'>Electrolux Digital UX Foundation</a></p>
+        <p className='detail'>Interaction Design, Prototyping</p>
+        <p className='title'><a href='#'>Blueair Interaction Design</a></p>
+        <p className='detail'>Interaction Design, UX Prototyping</p>
+        <p className='title'><a href='#'>Sellpy UX Redesign</a></p>
+        <p className='detail'>Inteaction Design, Prototyping</p>
+      </div>
+    </div>
+
+    <div className='container'>
+      <h4>Sides</h4>
+      <div className='content'>
+        <p className='title'><a href='https://www.behance.net/gallery/71168665/HIUT-Make-your-name-in-Korean'>Project Hiut</a></p>
+        <p className='detail'>Interation Design</p>
+        <p className='title'><a href='https://mid-spot.netlify.com/'>Mid-spot website</a></p>
+        <p className='detail'>Frontend Development</p>
+        <p className='title'><a href='https://followmove.netlify.com/'>Follow your move</a></p>
+        <p className='detail'>UI Development, two.js</p>
+        <p className='title'><a href='#'>CLO:CK:ED:IN</a></p>
+        <p className='detail'>Creative Coding, Processing, Arduino</p>
+      </div>
+    </div>
+
     <div className='container'>
       <h4>Experiences</h4>
       <div className='content'>
@@ -16,56 +50,19 @@ const IndexPage = () => (
         <p className='title'>Interactive Art Director, <a href='https://www.hyperisland.com/'>Hyper Island</a></p>
         <p className='detail'>2017.08 - 2018.06</p>
         <p className='title'>Creative Developer, <a href='http://innocean.com/ww-en/'>Innocean</a></p>
-        <p className='detail'>2016.11 - 2017.05</p>
-        <p className='title'>Digital Producer, <a href='http://innocean.com/ww-en/'>Innocean</a></p>
-        <p className='detail'>2015.01 - 2016.11</p>
+        <p className='detail'>2015.01 - 2017.05</p>
       </div>
       
-    </div>
-
-    
-
-    <div className='container'>
-      <h4>Works</h4>
-      <div className='content'>
-        <p className='title'><Link to="/wrc-real-play">Hyundai WRC Real Play</Link></p>
-        <p className='detail'>UX Design</p>
-        <p className='title'><a href='https://www.youtube.com/watch?v=6a97u3tkFQo'>Hyundai IONIQ Explore Plant VR</a></p>
-        <p className='detail'>Art Direction</p>
-        <p className='title'><a href='#'>CLO:CK:ED:IN</a></p>
-        <p className='detail'>Experience Design</p>
-        <p className='title'><a href='#'>Sellpy UX Redesign</a></p>
-        <p className='detail'>Interaction Design</p>
-      </div>
-    </div>
-
-    <div className='container'>
-      <h4>Experiments</h4>
-      <div className='content'>
-        <p className='title'><a href='https://followmove.netlify.com/'>Follow your move</a></p>
-        <p className='detail'>Two.js</p>
-        <p className='title'><a href='#'>Lost in Space</a></p>
-        <p className='detail'>Three.js + React</p>
-        <p className='title'><a href='#'>Type in Motion</a></p>
-        <p className='detail'>Framer</p>
-        <p className='title'><a href='#'>Glanceable Animation</a></p>
-        <p className='detail'>Framer</p>
-        
-      </div>
     </div>
 
     <div className='container'>
       <h4>Skills</h4>
       <div className='content-skill'>
-        <p>UX Research</p>
+        <p>User Research</p>
         <p>Interaction Design</p>
         <p>Prototyping</p>
-        <p>Creative Coding</p>
-        <p>Physical Computing</p>
-        <p>Javascript</p>
-        <p>Framer</p>
-        <p>React</p>
-        
+        <p>UX Engineering</p>
+        <p>Branding</p>
       </div>
     </div>
     
